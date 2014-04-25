406cb51af9856f0e800e5f9a2a028bfa177da6c3
Moises Delgado Valenzuela <moi153j@hotmail.com>
2014-04-25 12:04:49 +0200

    Agregando metodo leer archivo en memoria
    mirenlo

--- netfis/Clases/IONetfis.cs
+++ netfis/Clases/IONetfis.cs
@@ -14,11 +14,11 @@ namespace netfis.Clases
         {
             using (FileStream fs = File.OpenRead(ruta))
             {
-                using (StreamReader sr = new StreamReader(fs))
+                using (StreamReader sr = new StreamReader(fs, Encoding.UTF7))// poniendo Enconding.UTF7 te lee también ñ y tildes
                 {
                     while (sr.Peek() != -1)
                     {
-                        return sr.ReadLine();
+                        return sr.ReadToEnd();//poniendo ReadtoEnd te lee todo el archivo y ya no una sola linea
                     }
                     return null;
                 }
@@ -50,5 +50,34 @@ namespace netfis.Clases
 
             }
         }
+
+        //Lee archivos en memoria byte por byte
+        public void Memoria(string ruta)
+        {
+            //
+            try
+            {
+                using (FileStream Fs = new FileStream(ruta, FileMode.OpenOrCreate, FileAccess.Read))//crea un stream para abrir el archivo en modo lectura
+                {
+                    byte[] b = new byte[Fs.Length];//crea un array de bytes
+                    Fs.Read(b, (int)0, (int)Fs.Length);// lee byte por byte el stream y se pasa al array
+                    using (MemoryStream Ms = new MemoryStream(b))//crea un memorystream con el array para leer en memoria
+                    {
+
+                        System.Windows.MessageBox.Show(Encoding.UTF7.GetString(Ms.ToArray()));//muestra byte por byte el contenido del memorystream
+                        Ms.Close();
+
+                    }
+                    Fs.Close();
+                }
+            }
+            catch (Exception excepccion)
+            {
+
+                System.Windows.MessageBox.Show("Error, consulte a su Administrador de Sistema");
+            }
+            //
+        }
+
     }
 }
